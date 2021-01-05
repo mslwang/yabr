16726b4b8f707f2fcd9ad0cb071ffa1303ec3249
Hong Yi Chen <hongyigma@gmail.com>
2021-01-04 23:39:20 -0500

    Added basic react functional components

---
+++ client/src/components/SearchField/SearchField.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SearchField.css";
+
+const SearchField = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default SearchField;
---
+++ client/src/components/YabrApp/YabrApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./YabrApp.css";
+
+const YabrApp = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default YabrApp;
